0f6dda9478b7c081339cf2b6d8ed129e319003f9
siliconmagi <siliconmagi@yandex.com>
2017-09-12 19:40:08 -0700

    2017-09-12 19:40:08

--- strings.py
+++ strings.py
@@ -1,6 +1,40 @@
 # strings
-m1 = 'Hello World'
-m2 = 'Bobby\'s World'
+m1 = 'Hello Plane Earth'
+greeting = 'Hello'
+name = 'Silicon'
 
-print(m1)
-print(m2)
+# concat strings
+m2 = '\n' + greeting + ', ' + name + ' and Welcome!'
+
+# fstrings
+m3 = '\n{}, {}. Welcome!'.format(greeting, name)
+m4 = f'\n{greeting}, {name.upper()}. Welcome!'
+
+print(m1, m2, m3, m4)
+
+# slice
+print(m1[0:5])
+print(m1[6:])
+
+# lowercase
+print(m1.lower())
+print(m1.upper())
+
+# count
+print(m1.count('Hello'))
+print(m1.count('l'))
+
+# find message index
+print(m1.find('Plane'))
+# unknown values return '-1'
+print(m1.find('Globe'))
+
+# replace
+fe = m1.replace('Plane', 'Flat')
+print(fe)
+
+# print methods
+print(dir(name))
+
+# print help
+#  print(help(str))
