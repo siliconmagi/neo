38de345853c548965fb958455062b261b7645dee
siliconmagi <siliconmagi@yandex.com>
2017-12-03 08:24:37 -0800

    2017-12-03 08:24:37

---
+++ mail.py
@@ -0,0 +1,24 @@
+import smtplib
+
+from email.mime.text import MIMEText
+from config import me, pwd, to1, to2, cc1, cc2, out
+
+txt = './mail.txt'
+
+with open(txt, 'r') as fp:
+    msg = MIMEText(fp.read())
+
+server = smtplib.SMTP_SSL(out)
+server.set_debuglevel(1)
+server.login(me, pwd)
+
+sender = me
+recipient = [to1, to2]
+cc = [cc1, cc2]
+msg['Subject'] = 'Hourly Report 12/3/2017 8:00 AM'
+msg['From'] = me
+msg['To'] = ", ".join(recipient)
+msg['CC'] = ", ".join(cc)
+server.sendmail(me, me, msg.as_string())
+server.quit()
+print('done')
